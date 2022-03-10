9ac8aa0871a944c723627ab0f14f3e20a7dddf03
Warrick FitzGerald <warrick.fitzgerald@gmail.com>
2022-03-10 06:50:16 -0600

    1st commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { add2 } from "import-me"
+import {importme2add, mock} from "import-me2"
+console.log("Hello World!");
+console.log(add2(1,2));
+console.log(importme2add(1,2));
+// console.log(mock);
